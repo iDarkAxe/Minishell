cf89674cb73104ab9f8c68ef4376a291a8f26d49
Lucas D'Adamo <l.dadamopro@gmail.com>
2025-03-20 11:43:27 +0100

    finish to norm prompt

--- src/prompt.c
+++ src/prompt.c
@@ -6,18 +6,18 @@
 /*   By: lud-adam <lud-adam@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/18 17:24:58 by lud-adam          #+#    #+#             */
-/*   Updated: 2025/03/20 11:15:00 by lud-adam         ###   ########.fr       */
+/*   Updated: 2025/03/20 11:42:52 by lud-adam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+#include <fcntl.h>
+#include <readline/history.h>
+#include <readline/readline.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <readline/readline.h>
-#include <readline/history.h>
 #include <sys/stat.h>
-#include <fcntl.h>
 #include <unistd.h>
-#include "libft.h"
 
 static size_t	ft_strlen_and_choose_c(char *str, char c)
 {
@@ -37,15 +37,14 @@ static char	*read_prompt(char *prompt)
 	return (str);
 }
 
-static char	*get_hostname_join_message(char *username)
+static char	*get_hostname(void)
 {
-	char	*display;
 	char	*get_hostname;
-	char	buf[4096];
 	int		fd_hostname;
+	char	buf[4096];
 	size_t	buf_nbc;
 
-	fd_hostname = open("/etc/hostname", O_RDONLY); 
+	fd_hostname = open("/etc/hostname", O_RDONLY);
 	if (fd_hostname == -1)
 	{
 		perror("");
@@ -62,10 +61,21 @@ static char	*get_hostname_join_message(char *username)
 	get_hostname = ft_strndup(buf, ft_strlen_and_choose_c(buf, '.'));
 	if (!get_hostname)
 		return (NULL);
-	display = ft_strjoins((char *[]){username,"@", get_hostname, ":", getcwd(buf, 4096), "$ ", NULL});
+	return (get_hostname);
+}
+
+static char	*join_message(char *username)
+{
+	char	*display;
+	char	*hostname;
+	char	buf[4096];
+
+	hostname = get_hostname();
+	display = ft_strjoins((char *[]){username, "@", hostname, ":", getcwd(buf,
+				4096), "$ ", NULL});
 	if (!display)
 		return (NULL);
-	free(get_hostname);
+	free(hostname);
 	return (display);
 }
 
@@ -75,24 +85,23 @@ char	*get_prompt_message(void)
 	char	*prompt_message;
 
 	username = getenv("USER");
-	prompt_message = get_hostname_join_message(username);
+	prompt_message = join_message(username);
 	if (!prompt_message)
 		return (NULL);
 	return (prompt_message);
 }
 
-//
-// int		main(void)
+// int	main(void)
 // {
 // 	char	*prompt;
 // 	char	*display;
 //
-// 	display = get_prompt_message();	
+// 	display = get_prompt_message();
 // 	if (!display)
 // 		return (1);
 // 	while (1)
 // 		prompt = read_prompt(display);
 // 	free(display);
 // 	free(prompt);
-// 	return (0);	
+// 	return (0);
 // }
