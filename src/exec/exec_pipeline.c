bf5782e6e165e182d6335615473fb04c303a884d
Pierre Pontet <ppontet@student.42lyon.fr>
2026-02-12 12:01:00 +0100

    fix: possible invalid read from wstatus if waitpid == -1

--- src/exec/exec_pipeline.c
+++ src/exec/exec_pipeline.c
@@ -6,7 +6,7 @@
 /*   By: ppontet <ppontet@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/09 13:23:30 by ppontet           #+#    #+#             */
-/*   Updated: 2026/01/06 10:54:06 by ppontet          ###   ########lyon.fr   */
+/*   Updated: 2026/02/12 11:58:39 by ppontet          ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -113,6 +113,7 @@ void	wait_all_childs(t_command *command, pid_t *pids)
 	current = command;
 	while (index < nbr_cmds && current)
 	{
+		wstatus = 0;
 		if (waitpid(pids[index], &wstatus, 0) == -1)
 			perror("waitpid");
 		current->return_value = evaluate_status_code_as_ret(wstatus);
